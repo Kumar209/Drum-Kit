b0c8df15db815994de8f801f91fdb5a482b27359
Prashant Kumar Verma <76429886+Kumar209@users.noreply.github.com>
2022-04-28 20:26:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+var drumbtn = document.getElementsByClassName("drum");
+
+for (var i = 0; i < drumbtn.length; i++) {
+    drumbtn[i].addEventListener("click", function () {
+        makesound(this.innerHTML);
+        blinkstick(this.innerHTML);
+    });
+}
+
+
+document.addEventListener("keydown", function(event){
+    makesound(event.key);
+    blinkstick(event.key);
+});
+
+function makesound(key){
+    switch (key) {
+
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "k":
+            var kick = new Audio("sounds/kick-base.waw");
+            kick.play();
+            break;
+
+        case "l":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+    }
+
+}
+
+
+function blinkstick(currentkey){
+    
+    var keyblink=document.querySelector("."+currentkey);
+    keyblink.classList.add("pressed");
+
+    setTimeout(function(){
+        keyblink.classList.remove("pressed");
+    }, 1000);
+}
